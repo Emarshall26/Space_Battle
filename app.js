43c4b53ad4cacd8decebf81daa0fa3d3c8f58ff0
Edwina Marshall <edwinamarshall@PSADMINs-MacBook-Air.local>
2020-06-25 15:38:09 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// Project basics-
+//captain= USS Schwarzenegger
+//create 6 alien ships- weakness=onky attack 1 at a time
+//strength captain-attcak first
+//after destroying ship create option to make retreat
+
+
+//attack first ship if survive it attacks you
+//win if you destroy ship, option to restart of play again(reset)
+
+/*ship properties- 
+//hull if 0 or < ship is destroyed
+//firepower
